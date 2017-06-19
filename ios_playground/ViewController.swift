d4f1149cb4939dd0e37be3e8deab1aeec77984d1
nosaka <nosaka.shin@gmail.com>
2017-06-19 09:42:11 +0900

    Initial Commit

---
+++ ios_playground/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ios_playground
+//
+//  Created by Shintaro Nosaka on 2017/06/19.
+//  Copyright © 2017年 Shintaro Nosaka. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
